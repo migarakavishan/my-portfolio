6132692affc5e6f25b24cff430f48fea8ddd5026
migarakavishan <migarakavishan43@gmail.com>
2023-07-26 16:12:05 +0530

    add home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,42 +1,76 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Link } from "react-scroll";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
-    const [nav, setNav] = useState(false);
-    const link = [
-        {
-            id:1,
-            link: "home",
-        },
-        {
-            id:2,
-            link: "about",
-        },
-        {
-            id:3,
-            link: "portfolio",
-        },
-        {
-            id:4,
-            link: "skills",
-        },
-        {
-            id:5,
-            link: "contact",
-        },
-    ]
+  const [nav, setNav] = useState(false);
+  const link = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "portfolio",
+    },
+    {
+      id: 4,
+      link: "skills",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
   return (
-    <div className='flex justify-between items-center w-full h-20 px-20 text-white bg-black fixed py-10'>
-        <div>
-            <h1 className='text-5xl'>Migara</h1>
-        </div>
-        <ul className='hidden md:flex'>
-        {link.map(({id, link}) => (
-                <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-105 duration-200'><Link to={link} soomth duration={500}>{link}</Link></li>
-            ))} 
-        </ul>
-    </div>
-  )
-}
+    <div className="flex justify-between items-center w-full h-20 px-20 text-white bg-black fixed py-10">
+      <div>
+        <h1 className="text-5xl">Migara</h1>
+      </div>
+      <ul className="hidden md:flex">
+        {link.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-105 duration-200"
+          >
+            <Link to={link} soomth duration={500}>
+              {link}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
 
-export default NavBar
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-900 text-gray-400">
+          {link.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-3xl"
+            >
+              <Link
+                onClick={() => setNav(!nav)}
+                to={link}
+                smooth
+                duration={500}
+              >
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
