501ea1f9557fa6f07117e717d9c4097af1c58cad
migarakavishan <migarakavishan43@gmail.com>
2023-07-28 21:57:48 +0530

    homelitte

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
+import ProfileImage from "../assets/yellopro.png";
 
 const Home = () => {
   return (
@@ -10,26 +11,24 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white py-4">
-            I am Migara Kavishan{" "}
+          <h2 className="text-4xl sm:text-5xl font-bold text-white">
+            I'm a Ui/Ux designer and Frontend Developer
           </h2>
-          <h3 className="text-3xl sm:text-6xl text-gray-400">UI/UX designer</h3>
-          <h3 className="text-3xl sm:text-6xl text-gray-400">and</h3>
-          <h3 className="text-3xl sm:text-6xl text-gray-400">
-            Frontend developer
-          </h3>
           <p className="text-gray-500 py-4 max-w-md">
-            Hello! I'm a dedicated and ambitious UI/UX and Frontend Development
-            intern, eager to learn and grow in the world of design and
-            development. In this portfolio, I'm thrilled to share my projects
-            and experiences as I embark on this exciting journey into the realm
-            of creating captivating user experiences and bringing them to life
-            through frontend development.
+            Hello! I'm a Migara Kavishan and I am dedicated and ambitious UI/UX
+            and Frontend Development intern, eager to learn and grow in the
+            world of design and development.
           </p>
+
           <div>
-            <Link className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-600 to-blue-500 cursor-pointer">
-              Contact Me
-              <span className="group-hover:rotate-90 duration-500">
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </Link>
@@ -37,7 +36,11 @@ const Home = () => {
         </div>
 
         <div>
-          <img />
+          <img
+            src={ProfileImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-64 h-64 md:w-full max-w-md"
+          />
         </div>
       </div>
     </div>
