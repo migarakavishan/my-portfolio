5988c83416f8c2d42e69b540b06b0736c3ae7ffa
migarakavishan <migarakavishan43@gmail.com>
2023-12-15 00:53:56 +0530

    done

--- src/App.js
+++ src/App.js
@@ -6,11 +6,18 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import { Qualification } from "./components/Qualification";
-
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Migara Kavishan</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="testing" />
+
+      </Helmet>
       <NavBar />
       <Home />
       <About />
